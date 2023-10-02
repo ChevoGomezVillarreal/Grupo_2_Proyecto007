ceb7828e0b5864a0be6f8bba090f578fd08b511c
chevogomez-git <chevogomez-git@users.noreply.github.com>
2023-10-01 19:45:05 -0600

    Cambios al carrito para poder eliminar productos

--- public/js/productCart.js
+++ public/js/productCart.js
@@ -40,10 +40,11 @@ document.addEventListener('DOMContentLoaded',function(event){
     if(carrito!=undefined){
         let carrito2 = '[' + localStorage.getItem('carritoJson') + ']';
         let carritoJson = JSON.parse(carrito2);
+        let carritoTotal = document.getElementById('div-carrito-totales-cantidad');
         console.log('Carrito JSON =');
         console.log(carritoJson);
         let rowNum;
-        let descripcion="";
+        let totalCant=0;
         for( i = 1 ; i <= carritoJson.length ; i++ ){
             rowNum = 'carrito-product-row-' + i;
             
@@ -69,7 +70,9 @@ document.addEventListener('DOMContentLoaded',function(event){
             totalID = 'producto-total-' + i;
             let carritoTotal = document.getElementById(totalID);            
             carritoTotal.innerText = parseFloat(carritoJson[i-1].cantidad) * parseFloat(carritoJson[i-1].precio);             
+            totalCant = totalCant + (parseFloat(carritoJson[i-1].cantidad) * parseFloat(carritoJson[i-1].precio));
         }
+        carritoTotal.innerText = totalCant;
     } else {
         console.log('Carrito no esta definido');
         rowNum = 'carrito-product-row-1';
@@ -77,4 +80,83 @@ document.addEventListener('DOMContentLoaded',function(event){
         carritoRow.style.display = 'none';        
     }
 
+
+
 })
+
+function funEliminarArticulo(numArt){
+    console.log('Entre a eliminar articulo, el articulo a eliminar es el = ' + numArt);
+    
+    let articuloRow = document.getElementById('carrito-product-row-' + numArt);
+    let prodNombre = document.getElementById('producto-nombre-' + numArt);
+    let prodDescripcion = document.getElementById('producto-descripcion-' + numArt);
+    let prodPrecio = document.getElementById('producto-precio-' + numArt);
+    let prodCantidad = document.getElementById('producto-cantidad-' + numArt);
+    let prodTotal = document.getElementById('producto-total-' + numArt);
+
+    prodNombre.innerText = "---";
+    prodDescripcion.innerText = "---";
+    prodPrecio.innerText = "---";
+    prodCantidad.innerText = "---";
+    prodTotal.innerText = "0";
+
+    let totalID;
+    let carritoTotal;
+    let varCantidad =0;
+    let varTotal = 0;
+    for( i = 1 ; i <= 20 ; i++ ){
+        totalID = 'producto-total-' + (i);
+        console.log('id de articulo analizado = ' + totalID);
+        carritoTotal = document.getElementById(totalID);
+        varCantidad = parseFloat(carritoTotal.innerText); 
+        console.log('Cantidad a sumar = ' + varCantidad);  
+        articuloRow = document.getElementById('carrito-product-row-' + i);
+        if(articuloRow.style.display != 'none') {
+            varTotal = varTotal + varCantidad;  
+        }
+        console.log('Cantidad total sumada = ' + varTotal);   
+    }
+    
+    let totalText = document.getElementById('div-carrito-totales-cantidad');
+    totalText.innerText = varTotal;
+
+    let carrito=[];
+
+    let nuevoArticulo = {};
+
+    for( i = 1 ; i <= 20 ; i++ ){
+        totalID = 'producto-total-' + (i);
+        //console.log('id de articulo analizado = ' + totalID);
+        carritoTotal = document.getElementById(totalID);
+        varCantidad = parseFloat(carritoTotal.innerText); 
+        //console.log('Cantidad a sumar = ' + varCantidad);  
+        articuloRow = document.getElementById('carrito-product-row-' + i);
+        prodNombre = document.getElementById('producto-nombre-' + i);
+        prodDescripcion = document.getElementById('producto-descripcion-' + i);
+        prodPrecio = document.getElementById('producto-precio-' + i);
+        prodCantidad = document.getElementById('producto-cantidad-' + i);
+        prodTotal = document.getElementById('producto-total-' + i);        
+        
+        if(varCantidad == 0) {
+            articuloRow.style.display = 'none';  
+        } else {
+            nuevoArticulo = {
+                id: "10",
+                nombre: prodNombre.innerText,
+                descripcion: prodDescripcion.innerText,
+                precio: prodPrecio.innerText,
+                cantidad: prodCantidad.innerText
+            };
+            carrito.push(JSON.stringify(nuevoArticulo));
+
+        }
+  
+    }
+    console.log('El nuevo JSON es = :');
+    console.log(carrito); 
+
+    let carrito2 = JSON.stringify(carrito);
+    //carrito2 = carrito2.replace(/['"]+/g, '');
+    localStorage.setItem('carritoJson',carrito);
+
+}
